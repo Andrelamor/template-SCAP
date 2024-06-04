3f63cac83d87f7e22afa93b4f7e7b8b265ad5514
andrelamor <amorim.andrel@gmail.com>
2024-06-04 16:37:51 -0300

    Revert "add função de 'não-sobrescrever arquivo antigo' no script csv"
    This reverts commit bfe0e032497bc90a5dcea0a8ecc198ea1f6cae00.

--- scripts/convert_csv.py
+++ scripts/convert_csv.py
@@ -5,47 +5,46 @@ from unidecode import unidecode
 import config
 
 def find_all_files():
-  upload_files = os.listdir('upload')
-  for file in upload_files:
-    if file.endswith('.xlsx') or file.endswith('.xls'):
-      abas = config.abas.get(file)
-      if abas:
-        for aba in abas:
-          convert_csv(file, aba)
-      else:
-        convert_csv(file)
+    upload_files = os.listdir('upload')
+    for file in upload_files:
+        if file.endswith('.xlsx') or file.endswith('.xls'):
+            abas = config.abas.get(file)
+            if abas:
+                for aba in abas:
+                    convert_csv(file, aba)
+            else:
+                convert_csv(file)
 
 def convert_csv(file, aba=None):
-  read_file = pd.read_excel (f'upload/{file}', aba)
-  read_file = list(read_file.items())[0][1] if aba == None else read_file
-  read_file = read_file \
-      .replace('\n', ' ', regex=True) \
-      .replace('\r', '', regex=True)
-  columns = read_file.columns
-  new_columns = []
-  for column in columns:
-    new_columns.append(snake_small_case(column))
-  # import ipdb; ipdb.set_trace(context=10)
-  csv_name = file.split('.xls')[0]
-  csv_name = csv_name if aba == None else f'{aba}' # to xlsx and xls
-  read_file.columns=new_columns
+    read_file = pd.read_excel (f'upload/{file}', aba)
+    read_file = list(read_file.items())[0][1] if aba == None else read_file
+    read_file = read_file \
+                .replace('\n', ' ', regex=True) \
+                .replace('\r', '', regex=True)
+    columns = read_file.columns
+    new_columns = []
+    for column in columns:
+        new_columns.append(snake_small_case(column))
+    # import ipdb; ipdb.set_trace(context=10)
+    csv_name = file.split('.xls')[0]
+    csv_name = csv_name if aba == None else f'{aba}' # to xlsx and xls
+    read_file.columns=new_columns
 
-  # Remove white spaces from the beginning and the end of all columns
-  for column in new_columns:
-    read_file[column] = [read_file[column][i].strip() \
-                         if isinstance(read_file[column][i], str) \
-                         else read_file[column][i] \
-                         for i in range(0, len(read_file))]
+    # Remove white spaces from the beginning and the end of all columns
+    for column in new_columns:
+        read_file[column] = [read_file[column][i].strip() \
+                             if isinstance(read_file[column][i], str) \
+                             else read_file[column][i] \
+                             for i in range(0, len(read_file))]
 
-  # Append data to existing CSV (use 'a' mode for append)
-  read_file.to_csv (f'dataset/data/{csv_name}.csv', 
-                  index = None, 
-                  header= not os.path.exists(f'dataset/data/{csv_name}.csv'), # Add header only for new file
-                  sep = ';', 
-                  decimal = ',', 
-                  encoding = 'utf-8-sig', 
-                  na_rep = "",
-                  mode='a')  # Append mode
+    read_file.to_csv (f'dataset/data/{csv_name}.csv', \
+                    index = None, \
+                    header=True, \
+                    sep = ';', \
+                    decimal = ',', \
+                    encoding = 'utf-8-sig', \
+                    na_rep = ""\
+                    )
 
 def snake_small_case(column):
   column_lower = column.lower()
@@ -57,5 +56,4 @@ def snake_small_case(column):
   return column
 
 if __name__ == '__main__':
-  find_all_files()
-
+    find_all_files()
